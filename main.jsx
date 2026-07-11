a083989d893136ec31d5592946f764f8756568eb
kopacetix <riacirca84@gmail.com>
2026-07-11 12:57:36 -0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FireAlarmTrainer from "./FireAlarmTrainer.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FireAlarmTrainer />
-  </React.StrictMode>
-);
